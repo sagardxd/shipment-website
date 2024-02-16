1d3a9022f56e60f44c7f2a837a1acc649a2ce91d
Sagar Yadav <sagardxd5@gmail.com>
2024-02-16 21:17:52 +0530

    init

---
+++ backend/src/index.ts
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
