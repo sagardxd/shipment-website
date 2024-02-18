6889b2ed45fe635e36af7f9f0b5a1224492b25df
Sagar Yadav <sagardxd5@gmail.com>
2024-02-18 22:25:28 +0530

    login update

--- backend/src/routes/otpRoutes.ts
+++ backend/src/routes/otpRoutes.ts
@@ -1,16 +1,34 @@
 import express, { Request, Response } from 'express';
 import { sendOTP, verifyOTP, generateToken } from '../controllers/login';
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 const router = express.Router();
 
 router.post("/send-otp", async (req: Request, res: Response) => {
     const { phoneNumber } = req.body;
-    const otp = await sendOTP(phoneNumber);
 
-    if (otp) {
-        res.status(200).json({ message: "OTP sent successfully." });
-    } else {
-        res.status(500).json({ message: "Failed to send OTP." });
+    try {
+        // Check if a user with the provided phone number already exists
+        const existingUser = await prisma.user.findUnique({
+            where: { phoneNumber:phoneNumber }
+        });
+
+        if (existingUser) {
+            return res.status(400).json({ message: "User with this phone number already exists." });
+        }
+
+        // If the user doesn't exist, proceed to send OTP
+        const otpSent = await sendOTP(phoneNumber);
+
+        if (otpSent) {
+            return res.status(200).json({ message: "OTP sent successfully." });
+        } else {
+            return res.status(500).json({ message: "Failed to send OTP." });
+        }
+    } catch (error) {
+        console.error("Error sending OTP:", error);
+        return res.status(500).json({ message: "Failed to send OTP." });
     }
 
 })
