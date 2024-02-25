2d0bc8625afa3ffc6ce785d54cd00a0630d80065
Sagar Yadav <sagardxd5@gmail.com>
2024-02-26 00:00:24 +0530

    alluser data

--- backend/src/routes/admin.ts
+++ backend/src/routes/admin.ts
@@ -1,8 +1,17 @@
 import express, { Request, Response, Router } from 'express';
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 const router = express.Router();
 
-
+router.get("/userData", async(req: Request,res: Response) => {
+    const allUserData = await prisma.user.findMany();
+    if(allUserData){
+        return res.status(200).json({allUserData: allUserData});
+    }else{
+        return res.json({error: "error while fetching"})
+    }
+})
 
 
 export default router;
