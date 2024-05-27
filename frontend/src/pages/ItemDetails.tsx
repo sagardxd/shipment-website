fcdf1cf8e66ec14e77a6bb090117eaba878344f4
Sagar Yadav <sagardxd5@gmail.com>
2024-05-27 13:25:59 +0530

    added tailwind

--- frontend/src/pages/ItemDetails.tsx
+++ frontend/src/pages/ItemDetails.tsx
@@ -3,59 +3,59 @@ import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 interface DataItem {
-  _id: string;
-  awbNumber: string;
-  bookingDate: string;
-  consigneeName: string;
-  destination: string;
-  destinationCity: string;
-  forwardingNo: string;
-  origin: string;
-  originCity: string;
-  receiverName: string;
-  status: string;
+    _id: string;
+    awbNumber: string;
+    bookingDate: string;
+    consigneeName: string;
+    destination: string;
+    destinationCity: string;
+    forwardingNo: string;
+    origin: string;
+    originCity: string;
+    receiverName: string;
+    status: string;
 }
 
 const ItemDetails = () => {
-  const { awbNumber } = useParams<{ awbNumber: string }>();
-  const [item, setItem] = useState<DataItem | null>(null);
+    const { awbNumber } = useParams<{ awbNumber: string }>();
+    const [item, setItem] = useState<DataItem | null>(null);
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchItem = async () => {
-      try {
-        const res = await axios.get(`http://localhost:3000/admin/details/${awbNumber}`);
-        setItem(res.data);
-      } catch (error) {
-        console.error("Error fetching item:", error);
-      }
-    };
+    useEffect(() => {
+        const fetchItem = async () => {
+            try {
+                const res = await axios.get(`http://localhost:3000/admin/details/${awbNumber}`);
+                setItem(res.data);
+            } catch (error) {
+                console.error("Error fetching item:", error);
+            }
+        };
 
-    fetchItem();
-  }, [awbNumber]);
+        fetchItem();
+    }, [awbNumber]);
 
 
 
-  if (!item) {
-    return <div>Loading...</div>;
-  }
+    if (!item) {
+        return <div>Loading...</div>;
+    }
 
-  return (
-    <div>
-      <p>AWB Number: {item.awbNumber}</p>
-      <p>Booking Date: {new Date(item.bookingDate).toLocaleDateString()}</p>
-      <p>Consignee Name: {item.consigneeName}</p>
-      <p>Destination: {item.destination}</p>
-      <p>Destination City: {item.destinationCity}</p>
-      <p>Forwarding No: {item.forwardingNo}</p>
-      <p>Origin: {item.origin}</p>
-      <p>Origin City: {item.originCity}</p>
-      <p>Receiver Name: {item.receiverName}</p>
-      <p>Status: {item.status}</p>
-      <button onClick={(e)=> {navigate(`/update/${item.awbNumber}`)}}>Update</button>
-    </div>
-  );
+    return (
+        <div>
+            <p>AWB Number: {item.awbNumber}</p>
+            <p>Booking Date: {new Date(item.bookingDate).toLocaleDateString()}</p>
+            <p>Consignee Name: {item.consigneeName}</p>
+            <p>Destination: {item.destination}</p>
+            <p>Destination City: {item.destinationCity}</p>
+            <p>Forwarding No: {item.forwardingNo}</p>
+            <p>Origin: {item.origin}</p>
+            <p>Origin City: {item.originCity}</p>
+            <p>Receiver Name: {item.receiverName}</p>
+            <p>Status: {item.status}</p>
+            <button onClick={(e) => { navigate(`/update/${item.awbNumber}`) }}>Update</button>
+        </div>
+    );
 };
 
 export default ItemDetails;
