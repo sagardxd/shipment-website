adbfbe80b895093ba2be18d5db8794523a15774c
Sagar Yadav <sagardxd5@gmail.com>
2024-05-29 19:40:57 +0530

    dashboard

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { cn } from "../utils/cn";
 
 export function NavbarDemo() {
   return (
-    <div className="relative w-full flex items-center justify-center">
+    <div className="relative h-10 flex items-center justify-center">
       <Navbar className="top-2" />
     </div>
   );
@@ -15,17 +15,15 @@ function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
     <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+      className={cn("fixed top-10 inset-x-0 max-w-lg mx-auto z-50", className)}
     >
       <Menu setActive={setActive} >
-        {/* <MenuItem setActive={setActive} active={active} item="Services">
+        <MenuItem setActive={setActive} active={active} item="Services">
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/E-Commerce">E-Commerce</HoveredLink>
-            <HoveredLink href="/Air-Ship">Air Ship</HoveredLink>
-            <HoveredLink href="/Ocean-Ship">Door to Door</HoveredLink>
-            <HoveredLink href="/Warehouse">WareHouse</HoveredLink>
+            <HoveredLink href="/E-Commerce">Track Shipment</HoveredLink>
+            <HoveredLink href="/Air-Ship">Shipping Rate</HoveredLink>
           </div>
-        </MenuItem> */}
+        </MenuItem>
         <MenuItem setActive={setActive} active={active} item="Service">
           <div className="  text-sm grid lg:grid-cols-2 sm:grid-cols-1  gap-10 p-4">
             <ProductItem
--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -2,12 +2,12 @@ import { NavbarDemo } from "../components/Navbar"
 
 const LandingPage = () => {
     return (
-        <div className="bg-black w-full h-screen">
+        <div className="bg-white w-full h-screen">
             <NavbarDemo />
-            <div className="flex flex-col justify-center items-center h-screen text-white ">
-                <h1 className="text-4xl tracking-tight md:text-6xl text-center text-transparent font-bold bg-clip-text bg-gradient-to-r from-blue-600 to-indigo-600 mb-3 md:mb-4"> SafeShip</h1>
-                <p className="text-3xl tracking-tight font-bold md:text-6xl text-center text-neutral-800 dark:text-neutral-200 px-4 pb-4 w-fit">Where every parecel matters!</p>
-                <div className="text-sm md:text-xl text-neutral-400 dark:text-neutral-300 mt-4 max-w-sm md:max-w-2xl text-center mx-auto px-2 __className_7df6af">SecureShip ensures the safety and timely delivery of your precious cargo, employing state-of-the-art security measures and efficient logistics solutions. With us, your shipments are in trusted hands, providing you with peace of mind and reliability every step of the way</div>
+            <div className="flex flex-col justify-center items-center h-screen text-white gap-2">
+                <h1 className="text-4xl tracking-tight md:text-6xl text-center text-transparent font-bold bg-clip-text bg-gradient-to-r from-[#000000] to-[#434343] "> SafeShip</h1>
+                <p className="text-3xl tracking-tight font-bold md:text-6xl text-center text-zinc-800  px-4  w-fit">Where every parecel matters!</p>
+                <div className="text-sm md:text-xl text-zinc-600 mt-4 max-w-sm md:max-w-2xl text-center mx-auto px-2 __className_7df6af">SecureShip ensures the safety and timely delivery of your precious cargo, employing state-of-the-art security measures and efficient logistics solutions.</div>
             </div>
 
             
